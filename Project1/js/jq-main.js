38fda41967aeac348f87da286eb33367e99da9ae
ArturJanik <sidsky@o2.pl>
2015-05-15 18:30:58 +0200

    Finished jquery version of Project 1

--- Project1/js/jq-main.js
+++ Project1/js/jq-main.js
@@ -1,178 +1,167 @@
-////////////////////////
-// Warmup functions
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+$(document).ready(function () {
+  "use strict";
+  ////////////////////////
+  // Warmup functions
 
-var my_max = function (ary) {
-  var maks = ary[0];
-  ary.forEach(function (i) {
-    if (i > maks) {
-      maks = i;
-    }
-  });
-  return maks;
-};
 
-var vowel_count = function (txt) {
-  var my_string = txt.split("");
-  var counter = 0;
+  var my_max = function (ary) {
+    var maks = ary[0];
+    $.each(ary, function (i, j) {
+      if (j > maks) { maks = j; }
+    });
+    return maks;
+  };
+
+  var vowel_count = function (txt) {
+    var my_string = txt.split("");
+    var counter = 0;
+    $.each(my_string, function (i, j) {
+      if (j.search(/[aeiouy]/i) !== -1) { counter += 1; }
+    });
+    return counter;
+  };
+
+  var reverse = function (txt) {
+    var newTxt = txt.split("").reverse().join("");
+    return newTxt;
+  };
+  ////////////////////////
+
+  //
+  // CALCULATOR
+  //
+
+  ////////////////////////
+  // Operations
   
-  my_string.forEach(function (j) {
-    if (j.search(/[aeiouy]/i) !== -1) {
-      counter += 1;
-    }
-  });
-  return counter;
-};
-
-var reverse = function (txt) {
-  var newTxt = txt.split("").reverse().join("");
-  return newTxt;
-};
-////////////////////////
-
-//
-// CALCULATOR
-//
-
-////////////////////////
-// Operations
-
-var add = function (a, b) {
-  return a + b;
-};
-
-var subtract = function (a, b) {
-  return a - b;
-};
-
-var multiply = function (a, b) {
-  return a * b;
-};
-
-var divide = function (a, b) {
-  return a / b;
-};
-
-////////////////////////
-// Variable declarations
-
-// all numerical buttons
-var numericButtons = document.getElementsByClassName("number");
-
-// first value
-var firstval = 0.0;
-
-// selected operation
-var chosenOperation = null;
-
-// currently displayed value
-var cache = 0;
-
-// element used as display
-var displayWindow = document.getElementById('calc-display');
-
-// type of button
-var lastPressed = null;
-
-// Debugger
-var logmetender = function() {
-  var ob = {
-    firstval: firstval,
-    chosenop: chosenOperation,
-    cache: cache
-  };
-  console.log(ob);
-};
-
-// Styling divs informing about chosen operation
-var clearInfoClasses = function() {
-  var info = document.getElementsByTagName('li');
-  for (var i = 0; i < info.length; i++) {
-    if ( info[i].className.match(/(?:^|\s)selecta(?!\S)/) ) {
-      info[i].className = info[i].className.replace( /(?:^|\s)selecta(?!\S)/g , '' );
+  var operations = {
+    add: function (a, b) {
+      return a + b;
+    },
+    subtract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
     }
   };
-};
-var operationInfo = function(x) {
-  clearInfoClasses();
-  var el = document.getElementsByClassName(x);
-  el[0].className += " selecta";
-};
 
-////////////////////////
-// Mechanics - event listeners
+  ////////////////////////
+  // Variable declarations
 
-// Numeric buttons
-for (var i = 0; i < numericButtons.length; i++) {
-  numericButtons[i].addEventListener("click", function (e) {
-    if (lastPressed === "count") {
-      resetAll();
-    }
+  // all numerical buttons
+  var numericButtons = $(".number");
+
+  // first value
+  var firstval = 0.0;
+
+  // selected operation
+  var chosenOperation = null;
+
+  // currently displayed value
+  var cache = 0;
+
+  // element used as display
+  var calcDisp = $('#calc-display');
+
+  // type of button
+  var lastPressed = null;
+
+  // Debugger
+  var logmetender = function () {
+    var ob = {
+      firstval: firstval,
+      chosenop: chosenOperation,
+      cache: cache
+    };
+    console.log(ob);
+  };
+
+  // Styling divs informing about chosen operation
+  var clearInfoClasses = function () {
+    $('li').removeClass("selecta");
+  };
+  var operationInfo = function (x) {
+    clearInfoClasses();
+    $("." + x).addClass("selecta");
+  };
+
+  // Reset
+  var resetAll = function () {
+    firstval = null;
+    chosenOperation = null;
+    cache = 0;
+    calcDisp.text("0");
+    clearInfoClasses();
+    lastPressed = null;
+  };
+  
+  ////////////////////////
+  // Mechanics - event listeners
+
+  // Numeric buttons
+  numericButtons.on("click", function (e) {
+    if (lastPressed === "count") { resetAll(); }
+
     var value = e.target.innerHTML;
-    
-    if (firstval === 0 && chosenOperation === "subtract" && displayWindow.innerHTML.search(/\-/) === -1) {
+
+    if (firstval === 0 && chosenOperation === "subtract" && calcDisp.text().search(/\-/) === -1) {
       value = value * -1;
     }
-    
+
     if (cache === 0 || cache === null) {
-      displayWindow.innerHTML = value;
+      calcDisp.text(value);
     } else {
-      displayWindow.innerHTML += value;
+      calcDisp.append(value);
     }
-    
-    cache = parseFloat(displayWindow.innerHTML);
+    cache = parseFloat(calcDisp.text());
     lastPressed = "num";
   });
-};
 
-// Add comma
-document.getElementById("comma").addEventListener("click", function (e) {
-  var value = e.target.innerHTML;
-  
-  if (cache === 0 && chosenOperation === "subtract") {
-    displayWindow.innerHTML = "-0.";
-    cache = toString(displayWindow.textContent);
-  } else if (cache === 0) {
-    displayWindow.innerHTML = "0.";
-    cache = displayWindow.textContent;
-  } else if (displayWindow.innerHTML.search(/\./) === -1) {
-    displayWindow.innerHTML += ".";
-    cache = parseFloat(displayWindow.textContent);
-  }
-  lastPressed = "com";
-});
+  // Add comma
+  $("#comma").on("click", function (e) {
+    var value = e.target.innerHTML;
 
-// Operations
-["add", "subtract", "multiply", "divide"].forEach(function (elem) {
-  document.getElementById(elem).addEventListener("click", function (e) {
-    chosenOperation = e.target.id;
-    firstval = firstval || cache;
-    cache = null;
-    operationInfo(elem);
-    lastPressed = "ope";
-  });
-});
-
-// Count
-var doOperation = function() {
-  if (chosenOperation) {
-    if (!cache) {
-      cache = parseFloat(firstval);
+    if (cache === 0 && chosenOperation === "subtract") {
+      calcDisp.text("-0.");
+    } else if (cache === 0) {
+      calcDisp.text("0.");
+    } else if (calcDisp.text().search(/\./) === -1) {
+      calcDisp.append(".");
     }
-    firstval = window[chosenOperation](parseFloat(firstval), parseFloat(cache));
-    firstval = Math.round(firstval * 100000) / 100000;
-    displayWindow.innerHTML = firstval;
-  }
-  lastPressed = "count";
-};
-document.getElementById("count").addEventListener("click", doOperation);
+    cache = calcDisp.text();
+    lastPressed = "com";
+  });
 
-// Reset calculator
-var resetAll = function() {
-  firstval = null;
-  chosenOperation = null;
-  cache = 0;
-  displayWindow.innerHTML = 0;
-  clearInfoClasses();
-  lastPressed = null;
-};
-document.getElementById("clr").addEventListener("click", resetAll);
+  // Operations
+  $.each(["add", "subtract", "multiply", "divide"], function (i, v) {
+    $("#" + v).on("click", function (e) {
+      chosenOperation = v;
+      firstval = firstval || cache;
+      cache = null;
+      operationInfo(v);
+      lastPressed = "ope";
+    });
+  });
+  
+  // Count
+  var doOperation = function () {
+    if (chosenOperation) {
+      cache = cache || parseFloat(firstval);
+      
+      firstval = operations[chosenOperation](parseFloat(firstval), parseFloat(cache));
+      firstval = Math.round(firstval * 100000) / 100000;
+      calcDisp.text(firstval);
+    }
+    lastPressed = "count";
+  };
+  $("#count").on("click", doOperation);
+
+  // Reset calculator
+  $("#clr").on("click", resetAll);
+});
