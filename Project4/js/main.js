ab8ab16124d98f955093bdf7067e4f598933e061
ArturJanik <sidsky@o2.pl>
2015-05-28 23:28:17 +0200

    Created base files for Project4

---
+++ Project4/js/main.js
@@ -0,0 +1,27 @@
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+
+$(document).ready(function () {
+  'use strict';
+  
+  /* MAIN GAME OBJECT */
+  var App = (function () {
+    
+    // CONSTANTS
+    
+    // VARIABLES
+    
+    // INIT FUNCTION
+    var initialize = function () {
+      
+    };
+    
+    $('body').on('click', function (e) {});
+    
+    return {
+      initialize: initialize
+    };
+  }());
+  
+  App.initialize();
+});
