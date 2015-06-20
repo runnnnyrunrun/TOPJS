c37d9f18ef2042f3f3c606199bc599b77348068b
ArturJanik <sidsky@o2.pl>
2015-06-21 00:02:43 +0200

    Project 6 - basic Minesweeper completed

--- Project6/js/main.js
+++ Project6/js/main.js
@@ -13,36 +13,42 @@ $(document).ready(function () {
       this.y = y;
       this.bomb = false;
       this.minesAround = 0;
+      this.isFlagged = false;
       this.isRevealed = false;
     };
         
     // VARIABLES
     var adjacent = [[-1, -1], [-1, 0], [0, -1], [1, 1], [1, 0], [0, 1], [1, -1], [-1, 1]],
         board = [],
-        currentAdjacentField,
         currentField,
         fieldNumber,
         gameField = $('#fields'),
         randomCounter,
+        splashScreen = $('<section id="splash" class="game-start"><p>PLAY</p></section>'),
+        splashContent = {
+          'lose': 'GAME OVER<br>CLICK TO PLAY AGAIN',
+          'win': 'CONGRATULATIONS!<br>FIELD CLEARED<br>CLICK TO PLAY AGAIN'
+        },
         tempBombs,
         tempField;
     
     // METHODS
     var countAdjacent,
         fieldOnBoard,
+        flagToggle,
         generateBoard,
         generateBombs,
         initialize,
-        isBomb,
         render,
         resetBoard,
         reveal,
-        revealEmpty;
+        revealEmpty,
+        winCheck;
     
     countAdjacent = function () {
-      $.each(tempBombs, function(i, bombFieldNumber) {
-        var x = board[bombFieldNumber].x;
-        var y = board[bombFieldNumber].y;
+      $.each(tempBombs, function (i, bombFieldNumber) {
+        var x = board[bombFieldNumber].x,
+            y = board[bombFieldNumber].y;
         
         $.each(adjacent, function (j, v) {
           if (x + v[0] >= 0 && x + v[0] < 9 && y + v[1] >= 0 && y + v[1] < 9) {
@@ -54,6 +60,12 @@ $(document).ready(function () {
     
     fieldOnBoard = function (x, y, v) {
       return (x + v[0] >= 0 && x + v[0] < 9 && y + v[1] >= 0 && y + v[1] < 9);
+    };
+    
+    flagToggle = function (field) {
+      fieldNumber = $(field).data('field-number');
+      board[fieldNumber].isFlagged = !board[fieldNumber].isFlagged;
+      $(field).toggleClass('flagged');
     };
     
     generateBoard = function () {
@@ -76,28 +88,24 @@ $(document).ready(function () {
       }
     };
     
-    isBomb = function (x, y) {
-      return board[x + y * 9].bomb;
-    };
-    
     render = function () {
       gameField.empty();
-      $.each(board, function(i, v) {
+      $.each(board, function (i, v) {
         tempField = $('<div>').data('field-number', i);
         if (!v.isRevealed) {
           tempField.addClass('hidden');
+          if (v.isFlagged) {
+            tempField.addClass('flagged');
+          }
         }
-        if (v.bomb) {
-          tempField.text('B');
+        if (v.bomb && v.isRevealed) {
+          tempField.addClass('failure');
         } else if (v.minesAround > 0) {
           tempField.text(v.minesAround);
         }
         gameField.append(tempField);
       });
-    };
-    
-    reveal = function (field) {
-      board[field].isRevealed = true;
+      gameField.append(splashScreen);
     };
     
     resetBoard = function () {
@@ -110,12 +118,16 @@ $(document).ready(function () {
       }
     };
     
+    reveal = function (field) {
+      board[field].isRevealed = true;
+    };
+    
     revealEmpty = function (field) {
       var x = board[field].x;
       var y = board[field].y;
       
       reveal(field);
-      $.each(adjacent, function(i, v) {
+      $.each(adjacent, function (i, v) {
         if (fieldOnBoard(x, y, v)) {
           var calculatedField = (x + v[0]) + (y + v[1]) * 9;
           currentField = board[calculatedField];
@@ -128,16 +140,12 @@ $(document).ready(function () {
           }
         }
       });
-//      $.each([-10, -9, -8, -1, 1, 8, 9, 10], function(i, v) {
-//        currentField = board[field + v];
-//        if (field + v >= 0 && field + v < 81 && !currentField.isRevealed && !currentField.bomb) {
-//          if (currentField.minesAround === 0) {
-//            revealEmpty(field + v);
-//          } else {
-//            reveal(field + v);
-//          }
-//        }
-//      });
+    };
+    
+    winCheck = function () {
+      if (gameField.find('div.hidden').length === 10) {
+        $('#splash').find('p').html(splashContent.win).end().fadeIn(2000);
+      }
     };
     
     // INIT FUNCTION
@@ -145,21 +153,31 @@ $(document).ready(function () {
       // Generate clean board
       generateBoard();
       // Add event listeners
-      gameField.on('click', 'div', function () {
+      $('#fields').on('click', '#splash', function (e) {
+        generateBoard();
+        $(this).fadeOut(1000);
+      });
+      gameField.on('click.gameMech', 'div.hidden', function () {
         $(this).removeClass('hidden');
         fieldNumber = $(this).data('field-number');
         if (!board[fieldNumber].bomb && board[fieldNumber].minesAround === 0) {
           revealEmpty(fieldNumber);
         } else if (board[fieldNumber].bomb) {
-          $(this).addClass('failure');
+          $.each(tempBombs, function(i, v) {
+            reveal(v);
+          });
+          $('#splash').find('p').html(splashContent.lose).end().fadeIn(2000);
         } else {
           reveal(fieldNumber);
         }
         render();
+        winCheck();
       });
       gameField.on('contextmenu', 'div', function (e) {
         e.preventDefault();
-        
+      });
+      gameField.on('contextmenu.gameMech', 'div.hidden', function (e) {
+        flagToggle(this);
       });
     };
     
