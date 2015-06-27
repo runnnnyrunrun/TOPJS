1974cb722da2b7cab27f8eaed6d58418737c940f
ArturJanik <sidsky@o2.pl>
2015-06-28 00:17:36 +0200

    Project 7 - Forms - added

---
+++ Project7/js/main.js
@@ -0,0 +1,38 @@
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+
+$(document).ready(function () {
+  'use strict';
+  
+  /* MAIN APP OBJECT */
+  var App = (function () {
+    
+    // FIELD CONSTRUCTOR
+    var MyObject = function (x, y) {
+      this.x = x;
+      this.y = y;
+    };
+        
+    // VARIABLES
+    var i;
+    
+    // METHODS
+    var doSomething,
+        initialize;
+    
+    doSomething = function () {
+      
+    };
+    
+    // INIT FUNCTION
+    initialize = function () {
+
+    };
+    
+    return {
+      init: initialize
+    };
+  }());
+  
+  App.init();
+});
