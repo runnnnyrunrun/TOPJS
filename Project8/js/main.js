3a30a5dde7054d7bba6dd7ad8e4d28cef6459aa3
ArturJanik <sidsky@o2.pl>
2015-06-29 23:13:26 +0200

    Project 8: file setup

---
+++ Project8/js/main.js
@@ -0,0 +1,26 @@
+/*jslint browser: true*/
+/*global $, jQuery, console*/
+
+$(document).ready(function () {
+  'use strict';
+  
+  var myApp = (function ($) {
+    
+    // VARIABLES
+    var x;
+    
+    // METHODS
+    var initialize;
+    
+    // INIT method
+    initialize = function () {
+      
+    };
+    
+    return {
+      init: initialize
+    };
+  }(jQuery));
+  
+  myApp.init();
+});
